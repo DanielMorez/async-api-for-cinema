3e85b9711042499f2b5d34213da3ba5301b4ecd3
Денис Дугинов <duginov@grechka.digital>
2022-11-27 15:01:54 +0400

    Added env

--- async-api/src/core/config.py
+++ async-api/src/core/config.py
@@ -1,21 +1,21 @@
 import os
 from logging import config as logging_config
 
+from pydantic.env_settings import BaseSettings
+from pydantic.networks import RedisDsn
+
 from core.logger import LOGGING
 
-# Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
-# Название проекта. Используется в Swagger-документации
-PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
 
-# Настройки Redis
-REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+class Settings(BaseSettings):
+    project_name: str
+    redis_dsn: RedisDsn
+    es_dsn: str
+    base_dir: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
-ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
-
-# Корень проекта
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    class Config:
+        case_sensitive = False
+        env_file = '../.env'
+        env_file_encoding = 'utf-8'
