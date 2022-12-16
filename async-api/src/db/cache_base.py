0bb112669f8ee93c70d41d8229c9c9f26d3469d5
Денис Дугинов <duginov@grechka.digital>
2022-12-17 03:40:36 +0400

    Added solid for cache

---
+++ async-api/src/db/cache_base.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class AsyncCacheStorage(ABC):
+    @abstractmethod
+    async def get(self, key: str, **kwargs):
+        pass
+
+    @abstractmethod
+    async def set(self, key: str, value: str, expire: int, **kwargs):
+        pass
