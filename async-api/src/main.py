74a3df7f2720800321fb5d2c0125119eda0fa9b4
Денис Дугинов <duginov@grechka.digital>
2022-12-19 23:20:54 +0400

    Reformatted by black

--- async-api/src/main.py
+++ async-api/src/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
-    openapi_tags=tags_metadata
+    openapi_tags=tags_metadata,
 )
 
 
