84671a660037c530827dd4ec6bd4f2786fa90b16
Денис Дугинов <duginov@grechka.digital>
2023-01-11 17:01:58 +0400

    Added User & Login history models

--- auth/app.py
+++ auth/app.py
@@ -4,12 +4,21 @@ from config import settings
 from db import db, init_db
 
 from models.user import User
+from models.login_history import LoginHistory
 
 app = Flask(__name__)
 
 init_db(app)
 app.app_context().push()
 db.create_all()
+
+user = User(login="test", password="test", email="test@mail.com")
+user.save()
+
+login_history = LoginHistory(
+    user_id=user.id, user_agent="Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1"
+)
+login_history.save()
 
 
 @app.route("/hello-world")
