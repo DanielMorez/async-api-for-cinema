923fc27865ffb8af84d0f56b8bf8b8d0c33745f6
Денис Дугинов <duginov@grechka.digital>
2023-01-11 17:01:58 +0400

    Added User & Login history models

---
+++ auth/models/login_history.py
@@ -0,0 +1,45 @@
+import uuid
+import datetime
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import backref
+
+from db import db
+
+
+class LoginHistory(db.Model):
+    __tablename__ = "login_histories"
+
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    user_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    user_agent = db.Column(db.String, nullable=True)
+    device = db.Column(db.String, nullable=True)
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.datetime.utcnow,
+        nullable=False
+    )
+
+    user = db.relationship("User", backref=backref("login_histories", uselist=False))
+
+    def __init__(self, user_id: UUID, user_agent=None, device=None):
+        self.user_id = user_id
+        self.user_agent = user_agent
+        self.device = device
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Login at {self.created_at.strftime('%d/%m/%Y, %H:%M:%S')}>"
--- auth/models/user.py
+++ auth/models/user.py
@@ -1,25 +1,37 @@
 import uuid
-from sqlalchemy import Column, String
+
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import EmailType
+from werkzeug.security import (
+    generate_password_hash
+)
+
 from db import db
 
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = Column(
+    id = db.Column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
         unique=True,
         nullable=False,
     )
-    login = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    email = db.Column(EmailType, unique=True, nullable=True)
+    login = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
-    def __init__(self, login, password):
+    def __init__(self, login, password, email=None):
         self.login = login
         self.password = password
+        self.email = email
+
+    def save(self):
+        self.password = generate_password_hash(self.password)
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
-        return f"<User {self.login}>"
+        return f"<User {self.login or self.email}>"
