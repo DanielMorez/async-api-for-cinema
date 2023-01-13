d2e887b8744185650271fc16e01c31f969469b59
lenchikts <avlida@rambler.ru>
2023-01-13 19:47:53 +0300

    Issue 77 - create superuser, port auth v.2.2 changed from 6000 to 5000 in env.local

--- auth/createsuperuser.py
+++ auth/createsuperuser.py
@@ -1,3 +1,6 @@
+import logging
+from http import HTTPStatus
+
 import click
 from flask import Blueprint
 
@@ -6,6 +9,7 @@ from models import User
 
 bp = Blueprint("superuser", __name__)
 
+logger = logging.getLogger(__name__)
 
 @bp.cli.command("create")
 @click.argument("login")
@@ -17,12 +21,16 @@ def createsuperuser(login, password, password_confirm, email=None):
     user_exist = db.session.query(User).filter(User.login == login).first()
     if user_exist:
         print("User already exists. Try another login")
-        return "205 Reset content"
+        return {"message": f"User with login {login} already exists"}, HTTPStatus.RESET_CONTENT
     if password_confirm == password:
         superuser = User(login=login, password=password, email=email)
-        superuser.save()
+        try:
+            superuser.save()
+        except Exception as error:
+            logger.error(error)
+            return {"message": "Something went wrong"}, HTTPStatus.INTERNAL_SERVER_ERROR
     else:
         print("Password and Password_confirm don't match. Try again")
-        return "401 Unauthorized"
+        return {"message": "Password and Password_confirm don't match"}, HTTPStatus.BAD_REQUEST
     print("Superuser is created successfully")
-    return "201 Created"
+    return {"message": "Superuser is created successfully"}, HTTPStatus.CREATED
