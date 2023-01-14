c960ee7382120deeb0bd668e44bd4bceb35c3606
lenchikts <avlida@rambler.ru>
2023-01-15 01:54:29 +0300

    createsuperuser with roles

--- auth/createsuperuser.py
+++ auth/createsuperuser.py
@@ -5,7 +5,8 @@ import click
 from flask import Blueprint
 
 from db import db
-from models import User
+from models.user import User
+from models.role import UserRoles, Role
 
 bp = Blueprint("superuser", __name__)
 
@@ -23,9 +24,13 @@ def createsuperuser(login, password, password_confirm, email=None):
         print("User already exists. Try another login")
         return {"message": f"User with login {login} already exists"}, HTTPStatus.RESET_CONTENT
     if password_confirm == password:
-        superuser = User(login=login, password=password, email=email)
         try:
+            superuser = User(login=login, password=password, email=email, is_superuser=True)
             superuser.save()
+            role = Role(name="Admin", can_create_update=True, can_read=True, can_delete=True)
+            role.save()
+            new_role_user = UserRoles(user_id=superuser.id, role_id=role.id)
+            new_role_user.save()
         except Exception as error:
             logger.error(error)
             return {"message": "Something went wrong"}, HTTPStatus.INTERNAL_SERVER_ERROR
