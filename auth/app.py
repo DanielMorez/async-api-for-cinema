8c71f0c4e8f2da9ac12a560347f977a7b8f2e441
Денис Дугинов <duginov@grechka.digital>
2023-01-21 16:53:27 +0400

    Production deploy with wsgi and nginx

--- auth/app.py
+++ auth/app.py
@@ -1,12 +1,22 @@
+import logging
+
 from flask_jwt_extended import JWTManager
 
 from config import settings
 from utils.app_factory import create_app
 
 
+logger = logging.getLogger(__name__)
+
+
 app = create_app(settings)
 jwt = JWTManager(app)
 
+urls = app.url_map.iter_rules()
+
+for i in urls:
+    logger.error(str(i))
 
 if __name__ == "__main__":
-    app.run()
+    from config import settings
+    app.run(host=settings.host, port=settings.port)
