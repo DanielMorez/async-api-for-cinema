714fb79e189aef78d331db4d539f2729925a56d3
977alex <68866663+977alex@users.noreply.github.com>
2023-02-08 20:27:12 +0300

    Revert "Add ClickHouse single node cluster with Kafka Engine. Closes #129, Closes #130."

--- ugc/sample_producer.py
+++ ugc/sample_producer.py
@@ -1,6 +1,6 @@
 import asyncio
 from contextlib import asynccontextmanager
-from datetime import datetime
+from datetime import datetime, timedelta
 from random import randint
 
 from adapters.broker import KafkaProducerClient
@@ -10,7 +10,9 @@ from core.config import settings
 @asynccontextmanager
 async def kafka_producer():
     print(f"{settings.broker.host}:{settings.broker.port}")
-    client = KafkaProducerClient(f"{settings.broker.host}:{settings.broker.port}")
+    client = KafkaProducerClient(
+        f"{settings.broker.host}:{settings.broker.port}"
+    )
     await client.startup()
     yield client
     await client.shutdown()
@@ -20,14 +22,16 @@ async def main(loop=None):
     async with kafka_producer() as producer:
         user_id = str(f"u{randint(1, 10)}")
         movie_id = str(f"m{randint(1, 10)}")
-        timestamp = datetime.now()
+        start = datetime.now()
+        finish = start + timedelta(seconds=randint(10, 100))
 
         dt_format = "%Y-%m-%d %H:%M:%S"
 
         message = {
             "user_id": user_id,
             "movie_id": movie_id,
-            "timestamp": timestamp.strftime(dt_format),
+            "start": start.strftime(dt_format),
+            "finish": finish.strftime(dt_format),
         }
         key = f"{user_id}:{movie_id}"
         # print(message, key)
