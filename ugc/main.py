1de96ed4d67cb08da07ac78a6ca1be0c3a5f928a
Денис Дугинов <duginov@grechka.digital>
2023-03-07 01:27:23 +0400

    Fix port on dev

--- ugc/main.py
+++ ugc/main.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8002,
+        port=settings.port,
         log_config=LOGGING,
         log_level=logging.DEBUG,
     )
